4128e2fd380dc2369ddf28c7e4e19c413172f1b9
avvirani <aakash.v.virani@intel.com>
2022-08-16 22:17:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ profiledir="$(dirname "$0")"
 
 command="cd /tmp && apk add wget unzip && \
   mkdir syslinux && \
-  wget -O syslinux-5.00.zip https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-5.00.zip && \
-  unzip syslinux-5.00.zip -d syslinux && \
+  wget -O syslinux-6.03.zip https://mirrors.edge.kernel.org/pub/linux/utils/boot/syslinux/syslinux-6.03.zip && \
+  unzip syslinux-6.03.zip -d syslinux && \
   cp syslinux/memdisk/memdisk memdisk && \
   chmod a+x memdisk && \
-  rm -rf syslinux-5.00.zip syslinux && cd -"
+  rm -rf syslinux-6.03.zip syslinux && cd -"
 
 if [ ! -z "${http_proxy}" ]; then
   docker run --rm --env http_proxy=${http_proxy} --env https_proxy=${https_proxy} --env HTTP_PROXY=${http_proxy} --env HTTPS_PROXY=${https_proxy} --env NO_PROXY=localhost,127.0.0.1 -v $profiledir:/tmp alpine:3.9 /bin/sh -c "$command"
